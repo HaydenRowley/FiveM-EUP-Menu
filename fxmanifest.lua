b1d2ad46709f67729668caf577b69ac1b4a5d09a
Hayden Rowley <hayden.rowley09@gmail.com>
2022-04-11 15:39:53 +0100

    Added Files

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+name 'eup-ui-fivem'
+description 'An improved EUP menu, designed for EUP 8.x'
+author 'John Adams'
+
+client_script "@NativeUI/NativeUI.lua"
+client_script "client.lua"
